4724b39c25a4cae752522e8019880c13f98c4347
Wallace <wallace.balaniuc@gmail.com>
2020-02-14 13:55:53 -0500

    Added fire rate to laser shots

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ public class PlayerController : MonoBehaviour
     // Variables
     public float speed;
     public float minX, maxX, minY, maxY;
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float fireRateDelay = 0.25f;
 
     private Rigidbody2D rBody;
+    private float timer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +31,21 @@ public class PlayerController : MonoBehaviour
         rBody.position = new Vector2(
             Mathf.Clamp(rBody.position.x, minX, maxX),
             Mathf.Clamp(rBody.position.y, minY, maxY));
+    }
+
+    void Update()
+    {
+        // 1. Check for user input
+        if(Input.GetAxis("Fire1") > 0 && timer > fireRateDelay)
+        {
+            // 2. Create the object (instantiate)
+            GameObject gObj = Instantiate(laser, laserSpawn.transform.position, laserSpawn.transform.rotation);
+            gObj.transform.Rotate(0, 0, -90);
+
+            // Reset timer
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
     }
 }
