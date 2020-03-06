7b18f9d64e8a6e9282b90879b94ae868c7723954
Wallace Balaniuc <wbalaniu@my.centennialcollege.ca>
2020-03-06 13:52:09 -0500

    Asteroids explode

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // Create an explosion and position it properly
+        Instantiate(explosion, transform.position, transform.rotation);
+        // Destroy "this" object
+        // Destroy the "other" object
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
